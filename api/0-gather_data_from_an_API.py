98625c5bfb92fa4adc90e92ff398c5a7fae2e0fe
Skaveza <s.mwachoni@alustudent.com>
2023-11-05 20:04:47 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,20 +7,15 @@ Module
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        sys.argv[1])
-    url_2 = "https://jsonplaceholder.typicode.com/users/{}/".format(
-        sys.argv[1])
-
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(sys.argv[1])
+    url_2 = "https://jsonplaceholder.typicode.com/users/{}/".format(sys.argv[1])
     response = requests.get(url)
     result = response.json()
     response_2 = requests.get(url_2)
     result_2 = response_2.json()
 
     item_2 = result_2.get('name')
-
     count = 0
     count_2 = 0
 
@@ -29,9 +24,10 @@ if __name__ == "__main__":
             count_2 += 1
         if item.get('completed') and item.get('userId') == int(sys.argv[1]):
             count += 1
-    print('Employee {} is done with tasks({}/{}):'.format(item_2,
-                                                          count, count_2))
+
+    print('Employee {} is done with tasks ({}/{})'.format(item_2, count, count_2))
 
     for item in result:
         if item.get('completed') and item.get('userId') == int(sys.argv[1]):
-            print("\t {}".format(item['title']))
+            print("\t{}".format(item['title']))
+
